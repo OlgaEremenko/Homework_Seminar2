594fa24851fc4cd0e96ae04d5b6df77ab151a20c
Olga <shalamova_o@inbox.ru>
2022-08-17 10:49:07 +0500

    Add 3 tasks

---
+++ task10/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Write("Введите трехзначное число ");
+int number = Convert.ToInt32 (Console.ReadLine ());
+
+int FindSecondDigit (int number)
+
+{
+    int firstDigit = number / 10;
+    int secondDigit = firstDigit % 10;
+    int result = secondDigit;
+    return result;
+}
+
+Console.WriteLine($"Обработанное число {FindSecondDigit(number)}");
+
---
+++ task13/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет. Решить без использования строк.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// Целочисленное, остаток от деления и цикл
+
+Console.Write("Введите число ");
+int InputNumber = Convert.ToInt32 (Console.ReadLine ());
+
+int findThirdDigit (int InputNumber)
+
+{
+    int CurrentNumber = InputNumber;
+    
+    while (CurrentNumber > 999) 
+        {
+            CurrentNumber = CurrentNumber / 10;
+        }
+        
+    int result = CurrentNumber % 10;
+    return result;
+}
+
+if (InputNumber < 100)
+    {
+    Console.WriteLine("Третьей цифры нет");
+    }
+else
+    {
+    Console.WriteLine($"Обработанное число {findThirdDigit(InputNumber)}");
+    }
